fd1d8d59bd9f08ce85165c9c3b61b4e27b5e9170
35d <nikke.934@gmail.com>
2019-07-10 13:12:03 +0900

    ✨ chapter 3

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,11 +1,16 @@
 package main.java.money;
 
 public class Dollar {
-    int amount;
+    private int amount;
     public Dollar(int amount) {
         this.amount = amount;
     }
     public Dollar times(int multipulier) {
         return new Dollar(amount * multipulier);
+    }
+
+    public boolean equals (Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
