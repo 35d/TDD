8fdbe18cb01a77841dac797eb7ffecdf2af851db
35d <nikke.934@gmail.com>
2019-08-14 13:11:17 +0900

    ✨ chapter 12 (P86)

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,10 +1,17 @@
 package main.java.money;
 
-abstract public class Money {
+public class Money implements Expression {
     protected int amount;
     protected String currency;
 
-    abstract Money times(int multiplier);
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    };
 
     String currency() {
         return currency;
@@ -13,15 +20,23 @@ abstract public class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass())
+                && currency().equals(money.currency())
                 ;
     }
 
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 }
